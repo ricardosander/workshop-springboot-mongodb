f9f766ac8bdf86c614a8874d1a12a42e0d4afda3
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-06-24 03:35:57 -0300

    Initial commit.

---
+++ src/main/java/br/com/ricardosander/workshopspringbootmongodb/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,12 @@
+package br.com.ricardosander.workshopspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+}
