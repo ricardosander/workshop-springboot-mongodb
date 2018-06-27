6b2b467efea8cd5fe324eccb29157ccfea2b3398
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-06-27 01:34:35 -0300

    Using DTO pattern.

--- src/main/java/br/com/ricardosander/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/br/com/ricardosander/workshopspringbootmongodb/resources/UserResource.java
@@ -2,6 +2,7 @@ package br.com.ricardosander.workshopspringbootmongodb.resources;
 
 import br.com.ricardosander.workshopspringbootmongodb.UserService;
 import br.com.ricardosander.workshopspringbootmongodb.domain.User;
+import br.com.ricardosander.workshopspringbootmongodb.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,13 @@ public class UserResource {
   private UserService userService;
 
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<List<User>> findAll() {
-    return ResponseEntity.ok().body(userService.findAll());
+  public ResponseEntity<List<UserDTO>> findAll() {
+
+    List<User> users = userService.findAll();
+    List<UserDTO> usersDTO =
+        users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+
+    return ResponseEntity.ok().body(usersDTO);
   }
 
 }
